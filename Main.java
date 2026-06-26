67a2cecd17a1b714797ddae504a6202567b38ed4
Erick <erick92007@gmail.com>
2026-06-26 16:27:53 -0500

    I had the scafolding for this assignment already but it was under the other repo so i just pu tthe solution and the scafolding in two seperagte repos (:

--- Main.java
+++ Main.java
@@ -1,46 +1,46 @@
-// stduent code
-
 public class Main {
     public static void main(String[] args) {
         double[] pace = {70.0, 75.0, 80.0, 85.0, 90.0, 95.0};
         double[] overallRatings = {72.0, 76.0, 81.0, 84.0, 89.0, 93.0};
-        // Create two double variables, weighty and learningRate, and set them both to 0.0
         double weight = 0.0;
-        double learningRate = 0.0;
+        double learningRate = 0.0001;
     
-    
-    // The final training loop will go here!
-    for (int i = 0; i < 100; i++) {
-        weight = weight - (learningRate * calculateGradient(pace, overallRatings, weight));
+        // --- The final training loop will go here! ---
+        // Hint: Put these print statements INSIDE your loop so you can watch the model learn!
+        // System.out.println("Weight: " + weight);
+        // System.out.println("Error: " + calculateError(pace, overallRatings, weight));
+        // ---------------------------------------------
+        
+        System.out.println("Final Weight: " + weight);
+        
+        for (int i = 0; i < pace.length; i++) {
+            System.out.println("Prediction: " + pace[i] * weight  + ", Overall Rating: " + overallRatings[i]);
         }
     }
-
-
 
     public static double calculateError(double[] pace, double[] overallRatings, double weight) {
-        //mean Squared Error
-        double totalError = 0.0;
         double error = 0.0;
-        for (int i = 0; i < overallRatings.length; i++) {
-            // calculate the error
-            error = ((pace[i] * weight) - overallRatings[i]);
-            //sqaure it
-            totalError += error * error;
-        }
+
+        // Iterate through all the players
+
+        // calculate the error
+           
+        // square it
+           
         // divide it by 6 (amount of players)
-        totalError = totalError / overallRatings.length;
-        return totalError;
+
+        return error;  
     }
         
-        
-        
     public static double calculateGradient(double[] pace, double[] overallRatings, double weight) {
-        double playerGradient = 0.0;
-    
-        for (int i = 0; i < overallRatings.length; i++) {
-                            //(prediction)         Raw Mistake          change amount             
-            playerGradient += ((pace[i] *  weight) - overallRatings[i]) * pace[i];
-        }
-        return playerGradient / overallRatings.length;
+        double gradient = 0.0;
+        
+        // Iterate through all the players
+
+        // calculate Gradient
+
+        // divide it by 6 (amount of players)
+                   
+        return gradient;
     }
 }
