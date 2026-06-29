923bd0be74de9fb93fcf1f89c2569660d6516380
Erick <erick92007@gmail.com>
2026-06-29 12:22:40 -0500

    Added comments and TODO labeling

--- Main.java
+++ Main.java
@@ -1,15 +1,22 @@
 public class Main {
     public static void main(String[] args) {
+        // Player's Speed
         double[] pace = {70.0, 75.0, 80.0, 85.0, 90.0, 95.0};
+
+        // Player's Overall Performance
         double[] overallRatings = {72.0, 76.0, 81.0, 84.0, 89.0, 93.0};
+
+
         double weight = 0.0;
         double learningRate = 0.0001;
     
-        // --- The final training loop will go here! ---
-        // Hint: Put these print statements INSIDE your loop so you can watch the model learn!
-        // System.out.println("Weight: " + weight);
-        // System.out.println("Error: " + calculateError(pace, overallRatings, weight));
-        // ---------------------------------------------
+       // This loop trains the model by adjusting the weight based on the calculated gradient and learning rate
+        for (int i = 0; i < 10; i++) {
+            // subtract the weight by the learningRate times the gradient
+            weight = /* TODO: subtract the weight by the learningRate times the gradient */
+            System.out.println("Weight: " + weight);
+            System.out.println("Error: " + calculateError(pace, overallRatings, weight));
+        }
         
         System.out.println("Final Weight: " + weight);
         
@@ -18,20 +25,26 @@ public class Main {
         }
     }
 
+    // TODO: Implement the calculation for the calculateError method 
+    // Here, you are finding the mean sqaured error between the pace stats and the actual overallRatings.
+    // Return the final error
     public static double calculateError(double[] pace, double[] overallRatings, double weight) {
         double error = 0.0;
 
-        // Iterate through all the players
+        // Iterate through the players 
 
         // calculate the error
            
-        // square it
+        // square the error
            
         // divide it by 6 (amount of players)
 
         return error;  
     }
-        
+    // TODO: Implement the calculation for the calculateGradient method 
+    // Here, you are using the predicted and actual overallRating to find the gradient (rate of change)
+    // of the error with respect to the weight
+    // Return the final gradient.    
     public static double calculateGradient(double[] pace, double[] overallRatings, double weight) {
         double gradient = 0.0;
         
